81d6c23272eef39da7e51e3698141dc47496d57a
JaamalL <vitkt20@gmail.com>
2025-11-12 18:00:09 +0200

    Change ControllerAdvice annotation to more specific RestControllerAdvice annotation

--- src/main/java/com/todolist/todolist/exception/GlobalExceptionHandler.java
+++ src/main/java/com/todolist/todolist/exception/GlobalExceptionHandler.java
@@ -6,14 +6,14 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-@ControllerAdvice
+@RestControllerAdvice
 public class GlobalExceptionHandler {
     public record ErrorResponse(
             LocalDateTime timestamp,
